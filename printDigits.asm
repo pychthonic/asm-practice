b196a8dd156b85508d8317f3d9c497634c6d18cb
stephen-wolfe <46033300+stephen-wolfe@users.noreply.github.com>
2018-12-20 18:50:25 -0500

    Update printDigits.asm

--- printDigits.asm
+++ printDigits.asm
@@ -22,9 +22,26 @@ section .data			; the lines until the next section will contain initialized vari
 				; it reads 'define num as a quadword containing the number 66690210'. this means
 				; the variable 'num' will use one quad word of space, which is 64 bits. if you're 
 				; using an x86_64 machine (this means '64-bits'. you're most likely using either 
-				; a 64-bit or a 32-bit machine). then your registers hold 64 bits each. Put any 
-				; decimal number here, up to the max number a 64-bit register can hold. Just replace
-				; the one I put ("66690210") with the number to print to screen.
+				; a 64-bit or a 32-bit machine), then your registers hold 64 bits each. So that's 
+				; conventient for this program, since it means the program will print any number
+				; that will fit in one of your registers. And what *that* means is that once we get 
+				; it to work, we can print out the numbers that are held in any register! But for
+				; now, all we're concentrating on is 'how does a computer take a number and print
+				; it to the computer screen?'
+				; If you want to toy with the program, try substituting another decimal number here, 
+				; up to the biggest number a 64-bit register can hold. Keep putting bigger numbers 
+				; in until you get a segmentation fault. The segmentation fault means that the 
+				; number you asked the computer to put in a register was too big. Just replace the 
+				; one I put  ("66690210") with the number you want to print to the screen. If 
+				; you're running linux, this entails downloading the .asm file, going to your 
+				; terminal, navigating to the directory you downloaded the file to, then typing, 
+				; without the single quotes:
+				; 'nasm -f elf64 printDigits.asm' followed by the enter key. 
+				; Then type 
+				; 'ld -o printDigits printDigits.o' followed by the enter key. 
+				; Then type 
+				; './printDigits' followed by the enter key. You will see the
+				; number appear at your command line interface.
 
 section .bss			; this means "This section will contain uninitialized data." Uninitialized data
 				; is variables whose values are not defined until the program runs. In this program,
