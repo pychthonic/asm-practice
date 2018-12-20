857e5ded8682056ef7f98cf67d97d9a7191ddfa0
stephen-wolfe <46033300+stephen-wolfe@users.noreply.github.com>
2018-12-20 17:08:32 -0500

    Update printDigits.asm

--- printDigits.asm
+++ printDigits.asm
@@ -1,7 +1,28 @@
-section .data
-	num dq 902106662244244 	; put any decimal number here, up to the max number a 64-bit register can hold.
+	; any words to the right of a semi-colon are called comments and ignored by the computer.
+	; this is my first heavily commented assembly program.
+	; in assembly, even basic things like printing letters and numbers to the screen require 
+	; lots of instructions. 
+	; In order to "print" i.e. send numbers, letters, punctuation marks, and other symbols to the screen,
+	; you need to convert each decimal digit into an ascii character. For example, if you're printing the 
+	; number 430 to the screen, you'll have to first convert the number '4' to an ascii character, print it 
+	; to the screen, then do the same for the number '3', then do the same for the number '0'.
+	
+	; More commentary coming soon. I'll add comments throughout the week. The goal is to have all lines 
+	; explained so that even someone with very little or no experience with assembly language programming 
+	; can see what each line does and understand what it means, or something close to it.
 
-section .bss
+
+section .data			 ; the lines until the next section will contain initialized variables.
+	num dq 66690210 	; 'num' is the name of the variable. 'dq' means define quad word, which is 
+				; telling the computer that the variable will use one quad word of space, which is 
+				; 64 bits. if you're using an x86_64 machine, then your registers hold 64 bits each.
+				; Put any decimal number here, up to the max number a 64-bit register can hold. 
+				; Just replace the one I put ("90210666") with the number to print to screen.
+
+section .bss			; this means "This section will contain uninitialized data." Uninitialized data
+				; is variables whose values are not defined until the program runs. In this program,
+				; an uninitialized variable called 'asciiByte' will be used to store digits before
+				; they're printed to screen. 
 	asciiByte resb 1
 
 section .text
