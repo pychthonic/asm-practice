2e313b0a04011163d14d15f1b84d58e66e8a1d52
stephen-wolfe <46033300+stephen-wolfe@users.noreply.github.com>
2018-12-29 22:13:06 -0500

    Update and rename print_hex_modified.asm to printHex.asm

--- printHex.asm
+++ printHex.asm
@@ -1,7 +1,7 @@
 ;;;; I modified the assembly program found on page 27 of Igor 
 ;;;; Zhirkov's "Low Level Programming", so that it doesn't print 
 ;;;; leading zeros and prints negative numbers.
-;;;;
+;;;; 
 
 section .data
 newline db 0xa
